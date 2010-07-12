796e4129f8783194c39672d8a7c76cf1010cf43f
Eirik Brandtzæg <eirikdb@gmail.com>
2010-07-12 23:47:12 +0200

    Fixed a query to get data

--- Sporverein/grails-app/controllers/sportvereinrgb/ErgebnisController.groovy
+++ Sporverein/grails-app/controllers/sportvereinrgb/ErgebnisController.groovy
@@ -2,139 +2,136 @@ package sportvereinrgb
 
 class ErgebnisController {
 
-   static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
-   def index = {
-       redirect(action: "list", params: params)
-   }
+    def index = {
+        redirect(action: "list", params: params)
+    }
 
-   def list = {
-       params.max = Math.min(params.max ? params.int('max') : 10, 100)
-       [ergebnisInstanceList: Ergebnis.list(params), ergebnisInstanceTotal: Ergebnis.count()]
-   }
+    def list = {
+        params.max = Math.min(params.max ? params.int('max') : 10, 100)
+        [ergebnisInstanceList: Ergebnis.list(params), ergebnisInstanceTotal: Ergebnis.count()]
+    }
 
-   def create = {
-       def ergebnisInstance = new Ergebnis()
-       ergebnisInstance.properties = params
-       return [ergebnisInstance: ergebnisInstance]
-   }
+    def create = {
+        def ergebnisInstance = new Ergebnis()
+        ergebnisInstance.properties = params
+        return [ergebnisInstance: ergebnisInstance]
+    }
 
-   def save = {
-       def ergebnisInstance = new Ergebnis(params)
-       if (ergebnisInstance.save(flush: true)) {
-           flash.message = "${message(code: 'default.created.message', args: [message(code: 'ergebnis.label', default: 'Ergebnis'), ergebnisInstance.id])}"
-           redirect(action: "show", id: ergebnisInstance.id)
-       }
-       else {
-           render(view: "create", model: [ergebnisInstance: ergebnisInstance])
-       }
-   }
+    def save = {
+        def ergebnisInstance = new Ergebnis(params)
+        if (ergebnisInstance.save(flush: true)) {
+            flash.message = "${message(code: 'default.created.message', args: [message(code: 'ergebnis.label', default: 'Ergebnis'), ergebnisInstance.id])}"
+            redirect(action: "show", id: ergebnisInstance.id)
+        }
+        else {
+            render(view: "create", model: [ergebnisInstance: ergebnisInstance])
+        }
+    }
 
-   def show = {
-       def ergebnisInstance = Ergebnis.get(params.id)
-       if (!ergebnisInstance) {
-           flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'ergebnis.label', default: 'Ergebnis'), params.id])}"
-           redirect(action: "list")
-       }
-       else {
-           [ergebnisInstance: ergebnisInstance]
-       }
-   }
+    def show = {
+        def ergebnisInstance = Ergebnis.get(params.id)
+        if (!ergebnisInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'ergebnis.label', default: 'Ergebnis'), params.id])}"
+            redirect(action: "list")
+        }
+        else {
+            [ergebnisInstance: ergebnisInstance]
+        }
+    }
 
-   def edit = {
-       def ergebnisInstance = Ergebnis.get(params.id)
-       if (!ergebnisInstance) {
-           flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'ergebnis.label', default: 'Ergebnis'), params.id])}"
-           redirect(action: "list")
-       }
-       else {
-           return [ergebnisInstance: ergebnisInstance]
-       }
-   }
+    def edit = {
+        def ergebnisInstance = Ergebnis.get(params.id)
+        if (!ergebnisInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'ergebnis.label', default: 'Ergebnis'), params.id])}"
+            redirect(action: "list")
+        }
+        else {
+            return [ergebnisInstance: ergebnisInstance]
+        }
+    }
 
-   def update = {
-       def ergebnisInstance = Ergebnis.get(params.id)
-       if (ergebnisInstance) {
-           if (params.version) {
-               def version = params.version.toLong()
-               if (ergebnisInstance.version > version) {
+    def update = {
+        def ergebnisInstance = Ergebnis.get(params.id)
+        if (ergebnisInstance) {
+            if (params.version) {
+                def version = params.version.toLong()
+                if (ergebnisInstance.version > version) {
 
-                   ergebnisInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'ergebnis.label', default: 'Ergebnis')] as Object[], "Another user has updated this Ergebnis while you were editing")
-                   render(view: "edit", model: [ergebnisInstance: ergebnisInstance])
-                   return
-               }
-           }
-           ergebnisInstance.properties = params
-           if (!ergebnisInstance.hasErrors() && ergebnisInstance.save(flush: true)) {
-               flash.message = "${message(code: 'default.updated.message', args: [message(code: 'ergebnis.label', default: 'Ergebnis'), ergebnisInstance.id])}"
-               redirect(action: "show", id: ergebnisInstance.id)
-           }
-           else {
-               render(view: "edit", model: [ergebnisInstance: ergebnisInstance])
-           }
-       }
-       else {
-           flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'ergebnis.label', default: 'Ergebnis'), params.id])}"
-           redirect(action: "list")
-       }
-   }
+                    ergebnisInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'ergebnis.label', default: 'Ergebnis')] as Object[], "Another user has updated this Ergebnis while you were editing")
+                    render(view: "edit", model: [ergebnisInstance: ergebnisInstance])
+                    return
+                }
+            }
+            ergebnisInstance.properties = params
+            if (!ergebnisInstance.hasErrors() && ergebnisInstance.save(flush: true)) {
+                flash.message = "${message(code: 'default.updated.message', args: [message(code: 'ergebnis.label', default: 'Ergebnis'), ergebnisInstance.id])}"
+                redirect(action: "show", id: ergebnisInstance.id)
+            }
+            else {
+                render(view: "edit", model: [ergebnisInstance: ergebnisInstance])
+            }
+        }
+        else {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'ergebnis.label', default: 'Ergebnis'), params.id])}"
+            redirect(action: "list")
+        }
+    }
 
-   def delete = {
-       def ergebnisInstance = Ergebnis.get(params.id)
-       if (ergebnisInstance) {
-           try {
-               ergebnisInstance.delete(flush: true)
-               flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'ergebnis.label', default: 'Ergebnis'), params.id])}"
-               redirect(action: "list")
-           }
-           catch (org.springframework.dao.DataIntegrityViolationException e) {
-               flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'ergebnis.label', default: 'Ergebnis'), params.id])}"
-               redirect(action: "show", id: params.id)
-           }
-       }
-       else {
-           flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'ergebnis.label', default: 'Ergebnis'), params.id])}"
-           redirect(action: "list")
-       }
-   }
-
-
-   def bestenliste = {
+    def delete = {
+        def ergebnisInstance = Ergebnis.get(params.id)
+        if (ergebnisInstance) {
+            try {
+                ergebnisInstance.delete(flush: true)
+                flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'ergebnis.label', default: 'Ergebnis'), params.id])}"
+                redirect(action: "list")
+            }
+            catch (org.springframework.dao.DataIntegrityViolationException e) {
+                flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'ergebnis.label', default: 'Ergebnis'), params.id])}"
+                redirect(action: "show", id: params.id)
+            }
+        }
+        else {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'ergebnis.label', default: 'Ergebnis'), params.id])}"
+            redirect(action: "list")
+        }
+    }
 
 
-
-        
-
-
-               flash.message = params.long('sportartAuswaehlen_id')
-               flash.message = params.long('altersklassehinzufüegen.id')
-               def ak = params.long('altersklassehinzufüegen.id')
-               def sa = params.long('sportartAuswaehlen_id')
-               def az = params.int('anzahl')
-
-
-
+    def bestenliste = {
+        def ak = params.long('altersklassehinzufüegen.id')
+        def sa = params.long('sportartAuswaehlen_id')
+        def az = params.int('anzahl')
 
         if (request.method == 'POST'){
-            
-            def query = Ergebnis.executeQuery ("select res.sportlerHinzufuegen.vorname , res.sportlerHinzufuegen.nachname from Ergebnis as res where sportartAuswaehlen.id ="+sa.toString()+" and altersklassehinzufüegen.id= "+ak.toString()).toList()
-            //query.setResultSetMapping()
+            def c = Ergebnis.createCriteria()
+            def results = c {
+                and {
+                    sportartAuswaehlen {
+                        like("id", sa)
+                    }
+                    altersklassehinzufüegen {
+                        like("id", ak)
+                    }
+                    min("sortierWert")                   
+                }
+                if (az) maxResults(az)
+                order("sortierWert", params.order ? params.order : "desc")
+                projections {
+                    distinct("sportartAuswaehlen")
+                    property("id")
+                    property("sportlerHinzufuegen")
+                    property("turnierHinzufuegen")
+                    property("ergebnisEintragen")
+                    property("sortierWert")
+                }
+            }
+            [ergebnisInstanceList: results]
+        }
+    }
 
-            flash.message = query.toString()
-
-            //flash.message = query.toString()
-
-        ([ergebnisInstanceList : query])
-
-
-   }}
- 
-   
-
-
-
-
-     def bestenlistensportler = {
+    def bestenlistensportler = {
 
         flash.message = params.long('sportlerHinzufuegen.id')
         flash.message = params.long('sportartAuswaehlen_id')
@@ -144,57 +141,57 @@ class ErgebnisController {
 
         if (request.method == 'POST'){
 
-               def criteria = Ergebnis.createCriteria();
-               def results = criteria {
+            def criteria = Ergebnis.createCriteria();
+            def results = criteria {
 
 
                 and{
-                      if(params.sportartAuswaehlen_id && params.sportartAuswaehlen_id != ''){
+                    if(params.sportartAuswaehlen_id && params.sportartAuswaehlen_id != ''){
 
 
-                       sportlerHinzufuegen {
-                           eq("id", params.long('sportlerHinzufuegen.id'))
+                        sportlerHinzufuegen {
+                            eq("id", params.long('sportlerHinzufuegen.id'))
 
-                       }
-                      }
+                        }
+                    }
 
-                        sportartAuswaehlen {
-                           eq("id", params.long('sportartAuswaehlen_id'))
+                    sportartAuswaehlen {
+                        eq("id", params.long('sportartAuswaehlen_id'))
 
-                       }
-
-
-                     }
+                    }
 
 
-               maxResults( params.int('anzahl') )
+                }
+
+
+                maxResults( params.int('anzahl') )
 
                 def einheitentemp = Disziplin.get(params.long('sportartAuswaehlen_id'))
 
-                    if(einheitentemp.einheit == 'm'){
+                if(einheitentemp.einheit == 'm'){
 
                     order("sortierWert" , "desc" )
-                    }
-                    if (einheitentemp.einheit == 'sec'){
+                }
+                if (einheitentemp.einheit == 'sec'){
                     order("sortierWert" , "asc" )
-                    }
-                    if (einheitentemp.einheit == 'h'){
+                }
+                if (einheitentemp.einheit == 'h'){
                     order("sortierWert" , "asc" )
-                    }
+                }
 
 
 
 
 
-               }
-               render(view:'bestenlistensportler',model:[ergebnisInstanceList : results])
+            }
+            render(view:'bestenlistensportler',model:[ergebnisInstanceList : results])
+
+        }
+
+
+
 
     }
-
-
-
-
-}
 
 
 }
--- Sporverein/grails-app/views/ergebnis/bestenliste.gsp
+++ Sporverein/grails-app/views/ergebnis/bestenliste.gsp
@@ -2,93 +2,69 @@
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
-<head>
-  <title></title>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <meta name="layout" content="main" />
+  <head>
+    <title></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="layout" content="main" />
   <g:set var="entityName" value="${message(code: 'ergebnis.label', default: 'Ergebnis')}" />
 </head>
 <body>
 
- <div class="nav">
-           <span class="menuButton"><a class="home" href="${createLink(uri: '/')}"><g:message code="default.home.label"/></a></span>
-           <span class="menuButton"><g:link class="create" action="create"><g:message code="default.new.label" args="[entityName]" /></g:link></span>
-           <span class="menuButton"><g:link class="bestenliste" action="bestenliste"><g:message code="Bestenliste" args="[entityName]" /></g:link></span>
-           <span class="menuButton"><g:link class="bestenlistensportler" action="bestenlistensportler"><g:message code="Ergenisse nach Sportler" args="[entityName]" /></g:link></span>
- </div>
+  <div class="nav">
+    <span class="menuButton"><a class="home" href="${createLink(uri: '/')}"><g:message code="default.home.label"/></a></span>
+    <span class="menuButton"><g:link class="create" action="create"><g:message code="default.new.label" args="[entityName]" /></g:link></span>
+    <span class="menuButton"><g:link class="bestenliste" action="bestenliste"><g:message code="Bestenliste" args="[entityName]" /></g:link></span>
+    <span class="menuButton"><g:link class="bestenlistensportler" action="bestenlistensportler"><g:message code="Ergenisse nach Sportler" args="[entityName]" /></g:link></span>
+  </div>
   <br>
 
 
 <g:form action="bestenliste" method="post" >
 
 
-   <g:select name="sportartAuswaehlen_id" from="${sportvereinrgb.Disziplin.list()}" optionKey="id" value="${flash.message}"  />
-   <g:if test="${flash.message}"><div class="message">${flash.message}</div></g:if>
+  <g:select name="sportartAuswaehlen_id" from="${sportvereinrgb.Disziplin.list()}" optionKey="id" value="${flash.message}"  />
+  <g:if test="${flash.message}"><div class="message">${flash.message}</div></g:if>
 
 
 
-   <g:select name="altersklassehinzufüegen.id" from="${sportvereinrgb.Altersklasse.list()}" optionKey="id" value="${flash.message}"  />
-   <g:if test="${flash.message}"><div class="message">message: ${flash.message}</div></g:if>
+  <g:select name="altersklassehinzufüegen.id" from="${sportvereinrgb.Altersklasse.list()}" optionKey="id" value="${flash.message}"  />
+  <g:if test="${flash.message}"><div class="message">message: ${flash.message}</div></g:if>
 
 
-      <select name ="anzahl">
-        <option value="5"> 5 Ergebnisse anzeigen</option>
-        <option value="10"> 10 Ergebnisse anzeigen</option>
-        <option value="15"> 15 Ergebnisse anzeigen</option>
-        <option value="20"> 20 Ergebnisse anzeigen</option>
-        <option value="25"> 25 Ergebnisse anzeigen</option>
-      </select>
+  <select name ="anzahl">
+    <option value="5"> 5 Ergebnisse anzeigen</option>
+    <option value="10"> 10 Ergebnisse anzeigen</option>
+    <option value="15"> 15 Ergebnisse anzeigen</option>
+    <option value="20"> 20 Ergebnisse anzeigen</option>
+    <option value="25"> 25 Ergebnisse anzeigen</option>
+  </select>
 
-     <input type="submit" value="Auswahl bestätigen" />
+  <input type="submit" value="Auswahl bestätigen" />
 
 </g:form>
 
 <table>
-                   <thead>
-                       <tr>
+  <thead>
+    <tr>
+  <g:sortableColumn property="id" title="${message(code: 'ergebnis.id.label', default: 'Id')}" />
+  <th><g:message code="ergebnis.sportlerHinzufuegen.label" default="Sportler" /></th>
+<th><g:message code="ergebnis.turnierHinzufuegen.label" default="Turnier" /></th>
+<g:sortableColumn property="ergebnisEintragen" title="${message(code: 'ergebnis.ergebnisEintragen.label', default: 'Ergebnis')}" />
 
-                           <g:sortableColumn property="id" title="${message(code: 'ergebnis.id.label', default: 'Id')}" />
+</tr>
+</thead>
+<tbody>
+<g:each in="${ergebnisInstanceList}" status="i" var="ergebnisInstance">
+  <tr class="${(i % 2) == 0 ? 'odd' : 'even'}">
+    <td><g:link action="show" id="${ergebnisInstance[1]}">${ergebnisInstance[1]}</g:link></td>
+  <td>${ergebnisInstance[2]}</td>
+  <td>${ergebnisInstance[3]}</td>
+  <td>${ergebnisInstance[4]}</td>
+  </tr>
+</g:each>
+</tbody>
+</table>
 
-                           <th><g:message code="ergebnis.sportlerHinzufuegen.label" default="Sportler" /></th>
-
-                            <th><g:message code="ergebnis.turnierHinzufuegen.label" default="Turnier" /></th>
-
-                            <th><g:message code="ergebnis.turnierHinzufuegen.label" default="Ort" /></th>
-
-                           <g:sortableColumn property="ergebnisEintragen" title="${message(code: 'ergebnis.ergebnisEintragen.label', default: 'Ergebnis')}" />
-
-
-
-                       </tr>
-                   </thead>
-                   <tbody>
-                   <g:each in="${ergebnisInstanceList}" status="i" var="ergebnisInstance">
-                       <tr class="${(i % 2) == 0 ? 'odd' : 'even'}">
-
-                           <td><g:link action="show" id="${ergebnisInstance.id}">${fieldValue(bean: ergebnisInstance, field: "id")}</g:link></td>
-
-                           <td>${fieldValue(bean: ergebnisInstance, field: "sportlerHinzufuegen")}</td>
-
-                            <td>${fieldValue(bean: ergebnisInstance, field: "turnierHinzufuegen")}</td>
-
-                            <td>${fieldValue(bean: ergebnisInstance, field: "turnierHinzufuegen.ort")}</td>
-
-                           <td>${fieldValue(bean: ergebnisInstance, field: "ergebnisEintragen")}</td>
-
-
-
-                       </tr>
-                   </g:each>
-                   </tbody>
-               </table>
-
-
-
-
-
-
-
-
-  </body>
+</body>
 </html>
 
